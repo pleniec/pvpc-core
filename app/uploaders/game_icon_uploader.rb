da95d25141564d272964ed3411185950833ea6f6
Piotr Leniec <leniecpiotr@gmail.com>
2015-06-09 17:08:01 +0200

    removed uploaders

--- app/uploaders/game_icon_uploader.rb
+++
@@ -1,6 +0,0 @@
-class GameIconUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave
-
-  process convert: :jpg
-  process resize_to_fit: [100, 100]
-end
--- app/uploaders/game_image_uploader.rb
+++
@@ -1,6 +0,0 @@
-class GameImageUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave
-
-  process convert: :jpg
-  process resize_to_fit: [400, 300]
-end
