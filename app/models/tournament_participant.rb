07362e7692c526655932ca727d624a4172ef00d9
Piotr Leniec <leniecpiotr@gmail.com>
2015-08-28 10:16:28 +0200

    tournament models relations

--- app/models/tournament_participant.rb
+++ app/models/tournament_participant.rb
@@ -1,2 +1,4 @@
 class TournamentParticipant < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :tournament_team
 end
--- app/models/tournament_team.rb
+++ app/models/tournament_team.rb
@@ -1,2 +1,4 @@
 class TournamentTeam < ActiveRecord::Base
+  belongs_to :tournament_setup
+  has_many :torunament_participants
 end
