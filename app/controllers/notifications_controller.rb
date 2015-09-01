0c934147f729567290af27a7c146e46040147931
Piotr Leniec <leniecpiotr@gmail.com>
2015-09-01 10:13:39 +0200

    notification authorization

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < APIController
   def check
-    Notification.check!(params[:notification_ids])
+    Notification.find(notification_ids).each { |n| authorize! :check, n }
+    Notification.where(id: notification_ids).update_all(checked: true)
     render nothing: true
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -10,11 +10,4 @@ class Notification < ActiveRecord::Base
   scope :user_id, ->(user_id) { where(user_id: user_id) }
   scope :created_at, ->(created_at) { where(created_at: created_at) }
   scope :checked, ->(checked) { where(checked: checked) }
-
-  def self.check!(notification_ids)
-    transaction do
-      Notification.find(notification_ids)
-      Notification.where(id: notification_ids).update_all(checked: true)
-    end
-  end
 end
