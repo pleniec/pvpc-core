ecd58dd9542ea7a4a496b2109fafac40aa0da543
Piotr Leniec <leniecpiotr@gmail.com>
2015-09-09 15:27:38 +0200

    games from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,3 @@
 require 'factory_girl_rails'
 
-@users = FactoryGirl.create_list(:user, 30)
-@games = FactoryGirl.create_list(:game, 10)
-
-@users.each do |user|
-  5.times do
-    FactoryGirl.create(:game_ownership, user: user, game: @games.sample) rescue ActiveRecord::RecordInvalid
-  end
-
-  10.times do
-    FriendshipInvite.create!(from_user: user, to_user: @users.sample).accept! rescue ActiveRecord::RecordInvalid
-  end
-end
-
-5.times do
-  FactoryGirl.create(:team, founder: @users.sample)
-end
+FactoryGirl.create_list(:game, 10)
