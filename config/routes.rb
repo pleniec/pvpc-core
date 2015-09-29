12efe65ea45b3024ce4b3331998a372cfb2d78ae
Piotr Leniec <leniecpiotr@gmail.com>
2015-09-29 11:13:09 +0200

    removed admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,5 @@ Rails.application.routes.draw do
     resources :team_membership_propositions, only: [:index, :create, :destroy] do
       post :accept, on: :member
     end
-    resources :admin, path: '/admin/:model', only: [:index, :show, :create, :update, :destroy]
   end
 end
