e24027849b9f857a6118076ef91e96d5db4cf3ff
Piotr Leniec <leniecpiotr@gmail.com>
2015-10-06 14:55:47 +0200

    fixed teams controller specs

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,71 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe TeamsController do
-  before do
-    @user = FactoryGirl.create(:user)
-    @teams = FactoryGirl.create_list(:team, 3, founder: @user)
-  end
+  include_examples :authentication, restricted: {create: :post}, free: {index: :get, show: :get}
 
-  describe '#index' do
-    context 'with access_token parameter' do
-      it 'renders teams with current_user_relation' do
-        index access_token: @user.session.access_token
+  include_examples :create,
+                   permitted_params: ->(user) { FactoryGirl.build(:team, founder_id: user.id).attributes },
+                   forbidden_params: ->(user) { FactoryGirl.build(:team).attributes }
 
-        expect(response.status).to eql(200)
-        expect(response_body['models'].size).to eql(3)
-        response_body['models'].each do |hash|
-          expect(hash['current_user_relation']).not_to be_nil
-        end
-      end
-    end
+  include_examples :index,
+                   create_permitted_model: ->(user) { FactoryGirl.create(:team) },
+                   permitted_params: ->(model) { {name_like: model.name[0..2]} }
 
-    context 'without access_token parameter' do
-      it 'renders teams without current_user_relation' do
-        index
-
-        expect(response.status).to eql(200)
-        expect(response_body['models'].size).to eql(3)
-        response_body['models'].each do |hash|
-          expect(hash['current_user_relation']).to be_nil
-        end
-      end
-    end
-  end
-
-  describe 'GET #show' do
-    context 'with access_token paramter' do
-      it 'renders team' do
-        show access_token: @user.session.access_token, id: @teams[0].id
-
-        expect(response.status).to eql(200)
-        expect(response_body.keys.size).to be > 2
-      end
-    end
-
-    context 'without access_token parameter' do
-      it 'renders team' do
-        show id: @teams[0].id
-        
-        expect(response.status).to eql(200)
-        expect(response_body.keys.size).to be > 2
-      end
-    end
-  end
-
-  describe 'POST #create' do
-    it 'creates team' do
-      create access_token: @user.session.access_token,
-        name: 'tim', description: 'nice team', tag: 'hdp', founder_id: @user.id
-      expect(response.status).to eql(201)
-      expect(Team.count).to eql(4)
-    end
-
-    it 'cannot create team as other user' do
-      @other_user = FactoryGirl.create(:user)
-      create access_token: @user.session.access_token,
-        name: 'tim', description: 'nice team', tag: 'hdp', founder_id: @other_user.id
-      expect(response.status).to eql(403)
-      expect(Team.count).to eql(3)
-    end
-  end
+  include_examples :show,
+                   create_permitted_model: ->(user) { FactoryGirl.create(:team) }
 end
