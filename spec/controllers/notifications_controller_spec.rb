cf49b7353d97a3eb972c012873cf4effb9f16aaf
Piotr Leniec <leniecpiotr@gmail.com>
2015-10-07 11:21:31 +0200

    removed comment

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -25,38 +25,4 @@ RSpec.describe NotificationsController do
       end
     end
   end
-
-  #:user_id, :created_at, :checked, :limit, :offset
-=begin
-include_examples :index,
-               create_permitted_model: ->(user) { FactoryGirl.create(:game) },
-               permitted_params: ->(model) { {} }
-
-
-  describe '#index' do
-    before do
-      @users = FactoryGirl.create_list(:user, 5)
-      FriendshipInvite.create!(from_user: @users[1], to_user: @users[0])
-      FriendshipInvite.create!(from_user: @users[2], to_user: @users[0])
-      FriendshipInvite.create!(from_user: @users[3], to_user: @users[0]).accept!
-      FriendshipInvite.create!(from_user: @users[4], to_user: @users[0]).accept!
-    end
-
-    context 'without parameters' do
-      it 'renders forbidden status' do
-        index access_token: @users[0].session.access_token
-
-        expect(response.status).to eql 403
-      end
-    end
-
-    context 'with permitted user_id parameter' do
-      it "renders user's notifications" do
-        index access_token: @users[0].session.access_token, user_id: @users[0].id
-
-        expect(response_body['models'].size).to eql 6
-      end
-    end
-  end
-=end
 end
